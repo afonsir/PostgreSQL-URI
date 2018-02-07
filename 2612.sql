35b34b3ffc95b93ee9bed899ee41d13162da494a
afonsir <afonsir17@gmail.com>
2018-02-07 01:25:42 -0200

    2612 - Os Atores Silva

---
+++ 2612.sql
@@ -0,0 +1,1 @@
+select m.id, m.name from movies m join genres g on g.id = m.id_genres join movies_actors ma on m.id = ma.id_movies join actors a on a.id = ma.id_actors where a.name like 'Marcelo Silva' or a.name like 'Miguel Silva' and g.description like 'Action';
